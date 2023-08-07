fe31763ec4418dbb8b955f819f313f8aa6277c98
BatuhanEmre <batuhanemrekopan@gmail.com>
2023-08-07 18:28:18 +0300

    sepete ürün ekleme işlemi tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Container, Drawer, Group, Indicator, Input, SimpleGrid  } from "@mantine/core";
 import Card from "./components/Card";
 import { useState } from "react";
-import { List, ThemeIcon } from '@mantine/core';
+import { List,Badge } from '@mantine/core';
 import { IconCircleCheck } from '@tabler/icons-react';
 import { Button } from '@mantine/core';
 import { TbBasketFilled } from "react-icons/tb";
@@ -11,32 +11,38 @@ import { TbBasketFilled } from "react-icons/tb";
 
 const storeItems = [
   {
+    id:100,
     name: "Çanta",
     src:"/assets/images/bag.jpg",
     price: 20,
   },
   {
+    id:101,
     name: "Oyuncak Araba",
     src:"/assets/images/car.jpg",
     price: 20,
   },
   {
+    id:102,
     name: "Spor Ayakkabı",
     src:"/assets/images/shoe.jpg",
     price: 20,
   },
   {
+    id:103,
     name: "Klavye",
     src:"/assets/images/keyboard.jpg",
     price: 20,
   },
 
   {
+    id:104,
     name: "Müzik Sistemi",
     src:"/assets/images/speaker.jpg",
     price: 20,
   },
   {
+    id:105,
     name: "Kulaklık",
     src:"/assets/images/headphones.jpg",
     price: 20,
@@ -49,10 +55,24 @@ function App() {
   const [basketItems,setBasketItems] = useState([])
   const [searchValue,setSearchValue] = useState("")
   const [opened,setOpened] = useState(false)
-/*   const [opened, { open, close }] = useDisclosure(false); */
 
-  
   const filteredItems = storeItems.filter((item) => item.name.toLowerCase().indexOf(searchValue.toLowerCase()) >=0 );
+
+
+  const addToBasket = ({id,name}) => {
+    const basketIndex = basketItems.findIndex(item => item.id === id)
+    if(basketIndex>=0){
+      let _basketItems=[...basketItems];
+       _basketItems[basketIndex].count+=1;
+      setBasketItems(_basketItems)
+    }else{
+      setBasketItems([...basketItems,{id,name,count:1}])
+    }
+
+  }
+  
+
+
 
   return (
     <div className="App">
@@ -73,13 +93,13 @@ function App() {
 
       </Group> <br/>
         <SimpleGrid cols={3} className="store">
-          {filteredItems.map(({ name,src }) => {
+          {filteredItems.map(({ id,name,src }) => {
             return (
               <Card
                 key={name}
                 name={name}
                 src={src}
-                onAdd={() => setBasketItems([...basketItems,{name}])}
+                onAdd={() => addToBasket({id,name})}
               />
             );
           })}
@@ -93,15 +113,21 @@ function App() {
       size="sm"
       center
       icon={
-        <ThemeIcon color="teal" size={24} radius="xl">
+        <Badge color="teal" size={24} radius="xl">
           <IconCircleCheck size="1rem" />
-        </ThemeIcon>
+        </Badge>
       }
     >
       
       {
-        basketItems.map(({name,index}) => (
-          <List.Item key={index}> {name} </List.Item>
+        basketItems.map(({name,index,count}) => (
+          <List.Item key={index}>
+
+            <Group>  
+             {name}   
+            <Badge color="dark" variant="filled">{count}</Badge>  
+             </Group>
+              </List.Item>
         ))
       }
 
